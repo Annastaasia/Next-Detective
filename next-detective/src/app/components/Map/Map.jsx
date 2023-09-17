ab8dc0f7a142c600ee75757023cb126d24e35653
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2023-09-17 21:39:20 +0300

    feat: add map workimg

--- next-detective/src/app/components/Map/Map.jsx
+++ next-detective/src/app/components/Map/Map.jsx
@@ -1,22 +1,11 @@
 "use client";
 import React from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import style from "./map.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 import Vectorright from "../../../../public/vectorright.svg";
 
 export default function Map() {
-  const mapContainerStyle = {
-    width: "100%",
-    height: "600px",
-    saturation: -100,
-  };
-
-  const center = {
-    lat: -34.397,
-    lng: 150.644,
-  };
   return (
     <>
       <section className={style.container}>
@@ -34,15 +23,16 @@ export default function Map() {
           </Link>
         </div>
 
-        <LoadScript googleMapsApiKey="YOUR_API_KEY">
-          <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            center={center}
-            zoom={8}
-          >
-            <Marker position={center} />
-          </GoogleMap>
-        </LoadScript>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1501.0074724233007!2d37.6659021542763!3d55.74585413138761!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54aea4615b66d%3A0xfdd927b8ff05ae5e!2z0YPQuy4g0JDQu9C10LrRgdCw0L3QtNGA0LAg0KHQvtC70LbQtdC90LjRhtGL0L3QsCwgNDI!5e0!3m2!1sru!2sru!4v1694005383491!5m2!1sru!2sru"
+          style={{
+            width: "100%",
+            height: "600px",
+            border: "0px",
+            loading: "lazy",
+            referrerpolicy: "no-referrer-when-downgrade",
+          }}
+        ></iframe>
       </section>
     </>
   );
--- next-detective/src/app/components/MapContacts/MapContacts.jsx
+++ next-detective/src/app/components/MapContacts/MapContacts.jsx
@@ -1,24 +1,17 @@
 "use client";
 import React from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 export const MapContact = () => {
-    const mapContainerStyle = {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1501.0074724233007!2d37.6659021542763!3d55.74585413138761!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54aea4615b66d%3A0xfdd927b8ff05ae5e!2z0YPQuy4g0JDQu9C10LrRgdCw0L3QtNGA0LAg0KHQvtC70LbQtdC90LjRhtGL0L3QsCwgNDI!5e0!3m2!1sru!2sru!4v1694005383491!5m2!1sru!2sru"
+      style={{
         width: "100%",
         height: "100%",
-        saturation: -100,
-      };
-    
-      const center = {
-        lat: -34.397,
-        lng: 150.644,
-      };
-
-  return (
-    <LoadScript googleMapsApiKey="YOUR_API_KEY">
-      <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={8}>
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+        border: "0px",
+        loading: "lazy",
+        referrerpolicy: "no-referrer-when-downgrade",
+      }}
+    ></iframe>
   );
 };
